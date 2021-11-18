4d0786016bc370897dd73551091b9aade9e085ed
aporria <aporria0451@gmail.com>
2021-11-18 01:16:37 -0800

    idk

--- Target/src/Order.java
+++ Target/src/Order.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Order {
     private int orderID;
     private boolean confirmation;
@@ -35,5 +40,39 @@ public class Order {
 
     public void setDelMethod(String delMethod) {
         this.delMethod = delMethod;
+    }
+
+
+
+
+
+    public static void printOrderInfo(){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "4JForte2006#");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM mydb.order;");
+
+            while (resultSet.next()) {
+                System.out.println("Delivery Method: " + resultSet.getString("deliveryMethod"));
+                System.out.print("Confirmation: ");
+
+                if(resultSet.getInt("confirmation") == 1){
+                    System.out.print("Order Confirmed.\n");
+                }
+                else {
+                    System.out.println("Order Not Yet Confirmed.\n");
+                }
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printOrderInfo();
     }
 }
