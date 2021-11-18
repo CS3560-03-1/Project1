506338afdf0e5357359063f16087a25883482377
aporria <aporria0451@gmail.com>
2021-11-18 10:32:25 -0800

    added add method
    IT FUCKING WORKS

--- Target/src/JavaConnection.java
+++ Target/src/JavaConnection.java
@@ -1,8 +1,38 @@
 import java.sql.*;
 
-public class JavaConnection {
-    public static void main(String[] args){
+public class JavaConnection
+{
+    String driverClassName = "com.mysql.cj.jdbc.Driver";
+    String connectionUrl = "jdbc:mysql://localhost:3306/mydb";
+    String dbUser = "root";
+    String dbPwd = "4JForte2006#";
+
+    private static JavaConnection javaConnection = null;
+
+    private JavaConnection()
+    {
         try {
+            Class.forName(driverClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection(connectionUrl, dbUser, dbPwd);
+        return conn;
+    }
+
+    public static JavaConnection getInstance() {
+        if (javaConnection == null) {
+            javaConnection = new JavaConnection();
+        }
+        return javaConnection;
+    }
+}
+/*
+try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "jpacio123");
 
             Statement statement = connection.createStatement();
@@ -17,7 +47,4 @@ public class JavaConnection {
         catch(Exception e){
             e.printStackTrace();
         }
-
-
-    }
-}
+ */
--- Target/src/ProductItem.java
+++ Target/src/ProductItem.java
@@ -1,30 +1,24 @@
+import java.sql.*;
+
 public class ProductItem {
     private int prodID;
-    private float prodPrice, prodWeight;
+    //private float prodPrice, prodWeight;
     private boolean prodPerishable;
     private String prodDescription;
     private String prodName;
+    private String prodPhoto;
 
     public ProductItem()
     {
         prodID = 0;
-        prodPrice = 0.0F;
-        prodWeight = 0.0F;
         prodPerishable = false;
         prodDescription = "";
         prodName = "";
+        prodPhoto = "";
     }
 
     public int getProdID(){
         return prodID;
-    }
-
-    public float getProdPrice(){
-        return prodPrice;
-    }
-
-    public float getProdWeight(){
-        return prodWeight;
     }
 
     public boolean isProdPerishable(){
@@ -40,4 +34,39 @@ public class ProductItem {
         return prodName;
     }
 
+    public String getProdPhoto() {
+        return prodPhoto;
+    }
+
+    public void setProdID(int prodID) {
+        this.prodID = prodID;
+    }
+
+    public void setProdPerishable(boolean prodPerishable) {
+        this.prodPerishable = prodPerishable;
+    }
+
+    public void setProdDescription(String prodDescription) {
+        this.prodDescription = prodDescription;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public void setProdPhoto(String prodPhoto) {
+        this.prodPhoto = prodPhoto;
+    }
+
+    public static void main(String[] args)
+    {
+        ProductDAO product = new ProductDAO();
+        ProductItem prod = new ProductItem();
+        prod.setProdID(1017);
+        prod.setProdPerishable(true);
+        prod.setProdName("bananas");
+        prod.setProdPhoto("banana");
+        prod.setProdDescription("banana");
+        product.add(prod);
+    }
 }
